da445b052831e73c7a24706f161ce607a871733a
satheesh <satheesh.m5465@gmail.com>
2026-02-06 10:31:39 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,47 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-019715e0d74f695be"
+resource "aws_instance" "game_server" {
+  ami           = "ami-019715e0d74f695be" 
   instance_type = "t3.micro"
   key_name      = "jenkinskkp.pem"
 
-  vpc_security_group_ids = ["sg-07c6a5252b519a3c2"]
+  security_groups = [aws_security_group.game_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install nginx -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
 
   tags = {
-    Name = "jenkins-game-server"
+    Name = "Game-Server"
   }
 }
 
-output "public_ip" {
-  value = aws_instance.web.public_ip
+resource "aws_security_group" "game_sg" {
+  name = "game-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
